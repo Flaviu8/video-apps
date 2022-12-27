cbb530685462993f9dc997aef6035aabb187cf73
Flaviu8 <flavius_flaviu93@yahoo.com>
2022-12-27 10:13:00 +0000

    sign in

--- src/components/library/magic-client.js
+++ src/components/library/magic-client.js
@@ -1,12 +1,9 @@
 import { Magic } from "magic-sdk";
 
  const createMagic = () => {
-const MAGIC_API = String(process.env.REACT_APP_MAGIC_API)
-
-    const API = `pk_live_63BD2DCE060F4708`
 
     return ( typeof window !== "undefined" && 
-    new Magic(API) 
+    new Magic(process.env.REACT_APP_MAGIC_API_KEY) 
     );  // ✨
 };
 
