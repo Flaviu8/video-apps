ea970aa3acc94d8a3e247528d368b0b3015aa8f9
Flaviu8 <flavius_flaviu93@yahoo.com>
2022-12-27 10:27:39 +0000

    sign in

--- src/components/library/magic-client.js
+++ src/components/library/magic-client.js
@@ -2,7 +2,7 @@ import { Magic } from "magic-sdk";
 
  const createMagic = () => {
 
-const API_KEY = process.env.REACT_APP_MAGIC_API_KEY
+const API_KEY = (process.env.REACT_APP_MAGIC_API_KEY)
     return ( typeof window !== "undefined" && 
     new Magic (API_KEY) 
     );  // ✨
